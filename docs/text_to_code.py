ed19d0938b141fb81a5f69a1b2d5f7cac8060fb6
evitwilly <dmitry.tehnicov@gmail.com>
2022-05-21 11:35:23 +0300

    добавил скрипт, который превращает текстовый файл в код для моего приложения

---
+++ docs/text_to_code.py
@@ -0,0 +1,44 @@
+
+file = open("темная_пони.txt")
+lines = file.read().split("\n")
+file.close()
+
+chapters = [
+	{'id' : 1, 'title' : "1. Две подруги"},
+	{'id' : 2, 'title' : "2. Летние каникулы"},
+	{'id' : 3, 'title' : "3. Твайлайт Спаркл"},
+	{'id' : 4, 'title' : "4. Школа Кантерлота"},
+	{'id' : 5, 'title' : "5. Мистер Магитемз"},
+	{'id' : 6, 'title' : "6. Эмбер Грим"},
+	{'id' : 7, 'title' : "7. Вечеринка"},
+	{'id' : 8, 'title' : "8. Конец дружбы"},
+	{'id' : 9, 'title' : "9. Школа Дружбы"},
+	{'id' : 10, 'title' : "10. Магическая дуэль"},
+	{'id' : 11, 'title' : "11. Дружеская беседа"},
+	{'id' : 12, 'title' : "12. Извинения"},
+	{'id' : 13, 'title' : "13. Последняя беседа"},
+	{'id' : 14, 'title' : "14. Осенний забег"},
+	{'id' : 15, 'title' : "15. Тайна происхождения"},
+	{'id' : 16, 'title' : "16. Расследование"},
+	{'id' : 17, 'title' : "17. Оборотни"},
+	{'id' : 18, 'title' : "18. История оборотней"},
+	{'id' : 19, 'title' : "19. Колокольчик Грогара"},
+	{'id' : 20, 'title' : "20. Темная пони"}
+]
+
+filtered_lines = [x.strip() for x in lines if x]
+
+chapter = 0
+text = ""
+for line in filtered_lines:
+	if chapters[chapter]['title'] == line:
+		if chapter > 0:
+			text += "))\n"
+		text += "put({}, listOf(\n".format(chapters[chapter]['id'])
+		chapter += 1
+	else:
+		text += '"{}",\n'.format(line)
+
+code = open("code.txt", 'w')
+code.write(text)
+code.close()	
